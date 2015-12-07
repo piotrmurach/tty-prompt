1995f8084a8ba467ba75b54030f8c3064d6b8afd
Piotr Murach <pmurach@gmail.com>
2015-12-07 22:44:19 +0000

    Add validation example.

--- examples/ask.rb
+++ examples/ask.rb
@@ -5,3 +5,7 @@ require 'tty-prompt'
 prompt = TTY::Prompt.new
 
 prompt.ask('Do you like Ruby?', required: true, default: true)
+
+prompt.ask('What is your username?') do |q|
+  q.validate(/^[^\.]+\.[^\.]+/)
+end
