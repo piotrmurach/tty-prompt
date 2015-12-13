2a20771b721b08996dcc5393057ff18b2b2759bc
Piotr Murach <pmurach@gmail.com>
2015-12-13 14:54:39 +0000

    Add range example.

--- examples/ask.rb
+++ examples/ask.rb
@@ -9,3 +9,7 @@ prompt.ask('Do you like Ruby?', required: true, default: true)
 prompt.ask('What is your username?') do |q|
   q.validate(/^[^\.]+\.[^\.]+/)
 end
+
+prompt.ask('How do you like it on scale 1 - 10?', read: :int) do |q|
+  q.in('1-10')
+end
