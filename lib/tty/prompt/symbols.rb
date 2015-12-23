d481e897930e14a9a54e2ac09bf2530868aeff98
Piotr Murach <pmurach@gmail.com>
2015-12-24 00:04:36 +0100

    Change to split codes up.

---
+++ lib/tty/prompt/symbols.rb
@@ -0,0 +1,13 @@
+# encoding: utf-8
+
+module TTY
+  class Prompt
+    module Symbols
+      SPACE = " "
+      ITEM_SECURE     = "•"
+      ITEM_SELECTED   = "‣"
+      RADIO_CHECKED   = "⬢"
+      RADIO_UNCHECKED = "⬡"
+    end # Symbols
+  end # Prompt
+end # TTY
