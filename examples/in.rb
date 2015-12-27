be8452e0507447e6bcdaeb21c537280b668c1045
Piotr Murach <pmurach@gmail.com>
2015-12-27 13:02:29 +0100

    Change in example to short syntax.

--- examples/in.rb
+++ examples/in.rb
@@ -4,6 +4,4 @@ require 'tty-prompt'
 
 prompt = TTY::Prompt.new
 
-prompt.ask('How do you like it on scale 1 - 10?', read: :int) do |q|
-  q.in('1-10')
-end
+prompt.ask('How do you like it on scale 1 - 10?', in: '1-10')
