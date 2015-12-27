af7983083547dd3e56b1d8a785b0b8f5ebbc8fa2
Piotr Murach <pmurach@gmail.com>
2015-12-27 15:22:57 +0100

    Change echo spec.

--- spec/unit/question/echo_spec.rb
+++ spec/unit/question/echo_spec.rb
@@ -1,6 +1,6 @@
 # encoding: utf-8
 
-RSpec.describe TTY::Prompt::Question, '#read' do
+RSpec.describe TTY::Prompt::Question, '#echo' do
 
   subject(:prompt) { TTY::TestPrompt.new }
 
@@ -19,7 +19,7 @@ RSpec.describe TTY::Prompt::Question, '#read' do
   it 'asks with echo off' do
     prompt.input << "password"
     prompt.input.rewind
-    answer = prompt.ask("What is your password?") { |q| q.echo(false) }
+    answer = prompt.ask("What is your password?", echo: false)
     expect(answer).to eql("password")
     expect(prompt.output.string).to eq([
       "What is your password? ",
