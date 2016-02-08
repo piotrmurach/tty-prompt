6ddfedbb66e55b2ee3149bbdaa225754a33232ad
Piotr Murach <pmurach@gmail.com>
2016-02-08 22:10:30 +0000

    Change error message.

--- examples/in.rb
+++ examples/in.rb
@@ -4,4 +4,6 @@ require 'tty-prompt'
 
 prompt = TTY::Prompt.new
 
-prompt.ask('How do you like it on scale 1 - 10?', in: '1-10')
+prompt.ask('How do you like it on scale 1 - 10?', in: '1-10') do |q|
+  q.messages[:range?] = "Sorry wrong one!"
+end
