00b8d4364697fb1fd4fbffd2eed31787ad9f4e0a
Piotr Murach <pmurach@gmail.com>
2016-11-27 00:17:34 +0000

    Add complex example of ask.

--- examples/ask.rb
+++ examples/ask.rb
@@ -5,3 +5,11 @@ require 'tty-prompt'
 prompt = TTY::Prompt.new
 
 prompt.ask('What is your name?', default: ENV['USER'])
+
+prompt.ask('Folder name?') do |q|
+  q.required(true)
+  q.validate ->(v) { return !Dir.exist?(v) }
+  q.messages[:valid?] = 'Folder already exists?'
+  q.messages[:required?] = 'Folder name must not be empty'
+end
+
