62dc2811e731df4a073111f6ce3f44d7ad0cd35b
Piotr Murach <pmurach@gmail.com>
2016-11-27 22:23:56 +0000

    Change to detect function keys.

--- lib/tty/prompt/reader/key_event.rb
+++ lib/tty/prompt/reader/key_event.rb
@@ -21,10 +21,11 @@ module TTY
       #
       # @api public
       class KeyEvent < Struct.new(:value, :key)
-        META_KEY_CODE_RE = /^(?:\e)(O|N|\[|\[\[)(?:(\d+)(?:;(\d+))?([~^$])|(?:1;)?(\d+)?([a-zA-Z]))/
+        META_KEY_CODE_RE = /^(?:\e+)(O|N|\[|\[\[)(?:(\d+)(?:;(\d+))?([~^$])|(?:1;)?(\d+)?([a-zA-Z]))/
 
         def self.from(char)
           key = Key.new
+
           case char
           when Codes::RETURN   then key.name = :return
           when Codes::LINEFEED then key.name = :enter
@@ -33,9 +34,14 @@ module TTY
               "#{Codes::ESCAPE}#{Codes::BACKSPACE}",
               "#{Codes::ESCAPE}#{Codes::CTRL_H}"
             key.name = :backspace
+            key.meta = (char.chars[0] == Codes::ESCAPE)
           when Codes::DELETE   then key.name = :delete
-          when Codes::SPACE    then key.name = :space
-          when Codes::CTRL_C, Codes::ESCAPE then key.name = :escape
+          when Codes::SPACE, "#{Codes::ESCAPE}#{Codes::SPACE}"
+            key.name = :space
+            key.meta = (char.size == 2)
+          when Codes::ESCAPE, "#{Codes::ESCAPE}#{Codes::ESCAPE}"
+            key.name = :escape
+            key.meta = (char.size == 2)
           when proc { |c| c.length == 1 && c =~ /[a-z]/ }
             key.name = char
           when proc { |c| c.length == 1 && c =~ /[A-Z]/ }
@@ -44,15 +50,21 @@ module TTY
           when /^\d+$/
             key.name = :num
           when META_KEY_CODE_RE # ansi escape
-            key.meta = true
+            parts = META_KEY_CODE_RE.match(char)
+            code = "#{parts[1]}#{parts[2]}#{parts[4]}#{parts[6]}"
+            modifier = (parts[3] || parts[5] || 1) - 1
 
-            case char
+            key.ctrl = (modifier & 4) != 0
+            key.meta = (modifier & 10) != 0
+            key.shift = (modifier & 1) != 0
+
+            case code
             # f1 - f12
             when Codes::F1_XTERM, Codes::F1_GNOME, Codes::F1_WIN then key.name = :f1
             when Codes::F2_XTERM, Codes::F2_GNOME, Codes::F2_WIN then key.name = :f2
             when Codes::F3_XTERM, Codes::F3_GNOME, Codes::F3_WIN then key.name = :f3
             when Codes::F4_XTERM, Codes::F4_GNOME, Codes::F4_WIN then key.name = :f4
-            when Codes::F5 then key.name = :f5
+            when Codes::F5, Codes::F5_WIN then key.name = :f5
             when Codes::F6 then key.name = :f6
             when Codes::F7 then key.name = :f7
             when Codes::F8 then key.name = :f8
@@ -61,19 +73,24 @@ module TTY
             when Codes::F11 then key.name = :f11
             when Codes::F12 then key.name = :f12
             # navigation
-            when Codes::KEY_UP, Codes::KEY_UP_ALT, Codes::CTRL_K, Codes::CTRL_P
+            when Codes::KEY_UP, Codes::KEY_UP_XTERM, Codes::KEY_UP_SHIFT,
+                 Codes::CTRL_K, Codes::CTRL_P
               key.name = :up
-            when Codes::KEY_DOWN, Codes::KEY_DOWN_ALT, Codes::CTRL_J, Codes::CTRL_N
+            when Codes::KEY_DOWN, Codes::KEY_DOWN_XTERM, Codes::KEY_DOWN_SHIFT,
+                 Codes::CTRL_J, Codes::CTRL_N
               key.name = :down
-            when Codes::KEY_RIGHT, Codes::KEY_RIGHT_ALT, Codes::CTRL_L
+            when Codes::KEY_RIGHT, Codes::KEY_RIGHT_XTERM,
+                Codes::KEY_RIGHT_SHIFT, Codes::CTRL_L
               key.name = :right
-            when Codes::KEY_LEFT, Codes::KEY_LEFT_ALT, Codes::CTRL_H
+            when Codes::KEY_LEFT, Codes::KEY_LEFT_XTERM,
+                 Codes::KEY_LEFT_SHIFT, Codes::CTRL_H
               key.name = :left
-            when Codes::KEY_CLEAR, Codes::KEY_CLEAR_ALT
+            when Codes::KEY_CLEAR, Codes::KEY_CLEAR_XTERM,
+                 Codes::KEY_CLEAR_SHIFT
               key.name = :clear
-            when Codes::KEY_END, Codes::KEY_END_ALT
+            when Codes::KEY_END, Codes::KEY_END_XTERM
               key.name = :end
-            when Codes::KEY_HOME, Codes::KEY_HOME_ALT
+            when Codes::KEY_HOME, Codes::KEY_HOME_XTERM
               key.name = :home
             end
           end
