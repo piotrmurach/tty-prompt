f0284a311f1eab6fd091bcc78d2de448c745010f
Piotr Murach <pmurach@gmail.com>
2016-11-28 21:20:18 +0000

    Add vim bindings to select example.

--- examples/select.rb
+++ examples/select.rb
@@ -5,4 +5,14 @@ require 'tty-prompt'
 prompt = TTY::Prompt.new
 
 warriors = %w(Scorpion Kano Jax)
+
+prompt.on(:keypress) do |event|
+  if event.key.name == 'j'
+    prompt.publish(:keydown)
+  end
+  if event.key.name == 'k'
+    prompt.publish(:keyup)
+  end
+end
+
 prompt.select('Choose your destiny?', warriors)
