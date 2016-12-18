f9572e0165f036fa046ed1fbdc49cd1c5cf21063
Piotr Murach <pmurach@gmail.com>
2016-12-18 20:15:51 +0000

    Add pagination to #enum_select in reference to #23

--- lib/tty/prompt/enum_list.rb
+++ lib/tty/prompt/enum_list.rb
@@ -7,6 +7,8 @@ module TTY
     #
     # @api private
     class EnumList
+      PAGE_HELP = '(Press tab/right or left to reveal more choices)'.freeze
+
       # Create instance of EnumList menu.
       #
       # @api public
@@ -18,10 +20,16 @@ module TTY
         @active_color = options.fetch(:active_color) { @prompt.active_color }
         @help_color   = options.fetch(:help_color)   { @prompt.help_color }
         @error_color  = options.fetch(:error_color)  { @prompt.error_color }
+        @input        = nil
         @done         = false
+        @first_render = true
         @failure      = false
         @active       = @default
         @choices      = Choices.new
+        @per_page     = options[:per_page]
+        @page_help    = options[:page_help] || PAGE_HELP
+        @paginator    = EnumPaginator.new
+        @page_active  = @default
 
         @prompt.subscribe(self)
       end
@@ -31,6 +39,33 @@ module TTY
       # @api public
       def default(default)
         @default = default
+      end
+
+      # Set number of items per page
+      #
+      # @api public
+      def per_page(value)
+        @per_page = value
+      end
+
+      def page_size
+        (@per_page || Paginator::DEFAULT_PAGE_SIZE)
+      end
+
+      # Check if list is paginated
+      #
+      # @return [Boolean]
+      #
+      # @api private
+      def paginated?
+        @choices.size >= page_size
+      end
+
+      # @param [String] text
+      #   the help text to display per page
+      # @api pbulic
+      def page_help(text)
+        @page_help = text
       end
 
       # Set selecting active index using number pad
@@ -70,14 +105,15 @@ module TTY
       def call(question, possibilities, &block)
         choices(possibilities)
         @question = question
-        block.call(self) if block
+        block[self] if block
         setup_defaults
         render
       end
 
       def keypress(event)
         if [:backspace, :delete].include?(event.key.name)
-          @input.chop! unless @input.empty?
+          return if @input.empty?
+          @input.chop!
           mark_choice_as_active
         elsif event.value =~ /^\d+$/
           @input += event.value
@@ -94,24 +130,39 @@ module TTY
           @failure = true
         end
       end
-      alias_method :keyenter, :keyreturn
+      alias keyenter keyreturn
 
-      private
-
-      def mark_choice_as_active
-        if !@choices[@input.to_i - 1].nil?
-          @active = @input.to_i
+      def keyright(*)
+        if (@page_active + page_size) <= @choices.size
+          @page_active += page_size
         else
-          @active = nil
+          @page_active = 1
+        end
+      end
+      alias keytab keyright
+
+      def keyleft(*)
+        if (@page_active - page_size) >= 0
+          @page_active -= page_size
+        else
+          @page_active = @choices.size - 1
         end
       end
 
-      # Setup default option and active selection
+      private
+
+      # Find active choice or set to default
+      #
+      # @return [nil]
       #
       # @api private
-      def setup_defaults
-        validate_defaults
-        @active = @default
+      def mark_choice_as_active
+        if (@input.to_i > 1) && !@choices[@input.to_i - 1].nil?
+          @active = @input.to_i
+        else
+          @active = @default
+        end
+        @page_active = @active
       end
 
       # Validate default indexes to be within range
@@ -119,8 +170,16 @@ module TTY
       # @api private
       def validate_defaults
         return if @default >= 1 && @default <= @choices.size
-        fail PromptConfigurationError,
-             "default index `#{d}` out of range (1 - #{@choices.size})"
+        raise PromptConfigurationError,
+              "default index `#{d}` out of range (1 - #{@choices.size})"
+      end
+
+      # Setup default option and active selection
+      #
+      # @api private
+      def setup_defaults
+        validate_defaults
+        mark_choice_as_active
       end
 
       # Render a selection list.
@@ -134,9 +193,9 @@ module TTY
       def render
         @input = ''
         until @done
-          render_question
+          lines = render_question
           @prompt.read_keypress
-          refresh
+          refresh(lines)
         end
         render_question
         render_answer
@@ -153,9 +212,11 @@ module TTY
 
       # Determine area of the screen to clear
       #
+      # @param [Integer] lines
+      #   the lines to clear
+      #
       # @api private
-      def refresh
-        lines = @question.scan("\n").length + @choices.length + 2
+      def refresh(lines)
         @prompt.print(@prompt.clear_lines(lines))
         @prompt.print(@prompt.cursor.clear_screen_down)
       end
@@ -166,18 +227,34 @@ module TTY
       def render_question
         header = "#{@prefix}#{@question} #{render_header}"
         @prompt.puts(header)
-        return if @done
-        @prompt.print(render_menu)
-        @prompt.print(render_footer)
+        lines = header.lines.count
+        unless @done
+          menu = render_menu + render_footer
+          lines += menu.lines.count
+          @prompt.print(menu)
+        end
         render_error if @failure
+        render_page_help if paginated? && !@done
+        lines
       end
 
+      # Error message when incorrect index chosen
+      #
+      # @api private
+      def error_message
+        error = 'Please enter a valid number'
+        "\n" + @prompt.decorate('>>', @error_color) + ' ' + error
+      end
+
+      # Render error message and return cursor to position of input
+      #
       # @api private
       def render_error
-        error = 'Please enter a valid index'
-        @prompt.print("\n" + @prompt.decorate('>>', @error_color) + ' ' + error)
-        @prompt.print(@prompt.cursor.prev_line)
-        @prompt.print(@prompt.cursor.forward(render_footer.size))
+        @prompt.print(error_message)
+        if !paginated?
+          @prompt.print(@prompt.cursor.prev_line)
+          @prompt.print(@prompt.cursor.forward(render_footer.size))
+        end
       end
 
       # Render chosen option
@@ -188,7 +265,7 @@ module TTY
       def render_header
         return '' unless @done
         return '' unless @active
-        selected_item = "#{@choices[@active - 1].name}"
+        selected_item = @choices[@active - 1].name.to_s
         @prompt.decorate(selected_item, @active_color)
       end
 
@@ -201,6 +278,28 @@ module TTY
         "  Choose 1-#{@choices.size} [#{@default}]: #{@input}"
       end
 
+      # Pagination help message
+      #
+      # @return [String]
+      #
+      # @api private
+      def page_help_message
+        return '' unless paginated?
+        "\n" + @prompt.decorate(@page_help, @help_color)
+      end
+
+      # Render page help
+      #
+      # @api private
+      def render_page_help
+        @prompt.print(page_help_message)
+        if @failure
+          @prompt.print(@prompt.cursor.prev_line)
+        end
+        @prompt.print(@prompt.cursor.prev_line)
+        @prompt.print(@prompt.cursor.forward(render_footer.size))
+      end
+
       # Render menu with indexed choices to select from
       #
       # @return [String]
@@ -208,11 +307,11 @@ module TTY
       # @api private
       def render_menu
         output = ''
-        @choices.each_with_index do |choice, index|
+        @paginator.paginate(@choices, @page_active, @per_page) do |choice, index|
           num = (index + 1).to_s + @enum + Symbols::SPACE
           selected = Symbols::SPACE * 2 + num + choice.name
           output << if index + 1 == @active
-                      @prompt.decorate("#{selected}", @active_color)
+                      @prompt.decorate(selected.to_s, @active_color)
                     else
                       selected
                     end
--- spec/unit/enum_select_spec.rb
+++ spec/unit/enum_select_spec.rb
@@ -1,10 +1,10 @@
 # encoding: utf-8
 
 RSpec.describe TTY::Prompt do
-
-  subject(:prompt) { TTY::TestPrompt.new }
-
+  let(:page_help) {
+  }
   it "selects default option when return pressed immediately" do
+    prompt = TTY::TestPrompt.new
     choices = %w(/bin/nano /usr/bin/vim.basic /usr/bin/vim.tiny)
     prompt.input << "\n"
     prompt.input.rewind
@@ -16,13 +16,13 @@ RSpec.describe TTY::Prompt do
       "  2) /usr/bin/vim.basic\n",
       "  3) /usr/bin/vim.tiny\n",
       "  Choose 1-3 [1]: ",
-      "\e[1000D\e[K\e[1A" * 4,
-      "\e[1000D\e[K\e[J",
+      "\e[1000D\e[K\e[1A" * 4 + "\e[1000D\e[K\e[J",
       "Select an editor? \e[32m/bin/nano\e[0m\n"
     ].join)
   end
 
   it "selects option by index from the list" do
+    prompt = TTY::TestPrompt.new
     choices = %w(/bin/nano /usr/bin/vim.basic /usr/bin/vim.tiny)
     prompt.input << "3\n"
     prompt.input.rewind
@@ -34,18 +34,19 @@ RSpec.describe TTY::Prompt do
       "\e[32m  2) /usr/bin/vim.basic\e[0m\n",
       "  3) /usr/bin/vim.tiny\n",
       "  Choose 1-3 [2]: ",
-      "\e[1000D\e[K\e[1A\e[1000D\e[K\e[1A\e[1000D\e[K\e[1A\e[1000D\e[K\e[1A\e[1000D\e[K\e[J",
+      "\e[1000D\e[K\e[1A" * 4 + "\e[1000D\e[K\e[J",
       "Select an editor? \n",
       "  1) /bin/nano\n",
       "  2) /usr/bin/vim.basic\n",
       "\e[32m  3) /usr/bin/vim.tiny\e[0m\n",
       "  Choose 1-3 [2]: 3",
-      "\e[1000D\e[K\e[1A\e[1000D\e[K\e[1A\e[1000D\e[K\e[1A\e[1000D\e[K\e[1A\e[1000D\e[K\e[J",
+      "\e[1000D\e[K\e[1A" * 4 + "\e[1000D\e[K\e[J",
       "Select an editor? \e[32m/usr/bin/vim.tiny\e[0m\n"
     ].join)
   end
 
   it "selects option through DSL" do
+    prompt = TTY::TestPrompt.new
     prompt.input << "\n"
     prompt.input.rewind
     value = prompt.enum_select("Select an editor?") do |menu|
@@ -70,6 +71,7 @@ RSpec.describe TTY::Prompt do
   end
 
   it "selects option through DSL with key and value" do
+    prompt = TTY::TestPrompt.new
     prompt.input << "\n"
     prompt.input.rewind
     value = prompt.enum_select("Select an editor?") do |menu|
@@ -93,6 +95,7 @@ RSpec.describe TTY::Prompt do
   end
 
   it "changes colors for selection, hint and error" do
+    prompt = TTY::TestPrompt.new
     choices = %w(/bin/nano /usr/bin/vim.basic /usr/bin/vim.tiny)
     prompt.input << "\n"
     prompt.input.rewind
@@ -107,6 +110,157 @@ RSpec.describe TTY::Prompt do
       "\e[1000D\e[K\e[1A" * 4,
       "\e[1000D\e[K\e[J",
       "Select an editor? \e[31m/bin/nano\e[0m\n"
+    ].join)
+  end
+
+  it "displays error with unrecognized input" do
+    prompt = TTY::TestPrompt.new
+    choices = %w(/bin/nano /usr/bin/vim.basic /usr/bin/vim.tiny)
+    prompt.input << "11\n2\n"
+    prompt.input.rewind
+    value = prompt.enum_select("Select an editor?", choices)
+    expect(value).to eq('/usr/bin/vim.basic')
+    expect(prompt.output.string).to eq([
+      "Select an editor? \n",
+      "\e[32m  1) /bin/nano\e[0m\n",
+      "  2) /usr/bin/vim.basic\n",
+      "  3) /usr/bin/vim.tiny\n",
+      "  Choose 1-3 [1]: ",
+      "\e[1000D\e[K\e[1A" * 4 + "\e[1000D\e[K\e[J",
+      "Select an editor? \n",
+      "\e[32m  1) /bin/nano\e[0m\n",
+      "  2) /usr/bin/vim.basic\n",
+      "  3) /usr/bin/vim.tiny\n",
+      "  Choose 1-3 [1]: 1",
+      "\e[1000D\e[K\e[1A" * 4 + "\e[1000D\e[K\e[J",
+      "Select an editor? \n",
+      "\e[32m  1) /bin/nano\e[0m\n",
+      "  2) /usr/bin/vim.basic\n",
+      "  3) /usr/bin/vim.tiny\n",
+      "  Choose 1-3 [1]: 11",
+      "\e[1000D\e[K\e[1A" * 4 + "\e[1000D\e[K\e[J",
+      "Select an editor? \n",
+      "\e[32m  1) /bin/nano\e[0m\n",
+      "  2) /usr/bin/vim.basic\n",
+      "  3) /usr/bin/vim.tiny\n",
+      "  Choose 1-3 [1]: \n",
+      "\e[31m>>\e[0m Please enter a valid number",
+      "\e[A\e[1G\e[18C",
+      "\e[1000D\e[K\e[1A" * 4 + "\e[1000D\e[K\e[J",
+      "Select an editor? \n",
+      "  1) /bin/nano\n",
+      "\e[32m  2) /usr/bin/vim.basic\e[0m\n",
+      "  3) /usr/bin/vim.tiny\n",
+      "  Choose 1-3 [1]: 2\n",
+      "\e[31m>>\e[0m Please enter a valid number",
+      "\e[A\e[1G\e[19C",
+      "\e[1000D\e[K\e[1A" * 4 + "\e[1000D\e[K\e[J",
+      "Select an editor? \e[32m/usr/bin/vim.basic\e[0m\n"
+    ].join)
+  end
+
+  it "paginates long selections" do
+    prompt = TTY::TestPrompt.new
+    choices = %w(A B C D E F G H)
+    prompt.input << "\n"
+    prompt.input.rewind
+    value = prompt.enum_select("What letter?", choices, per_page: 3, default: 4)
+    expect(value).to eq('D')
+    expect(prompt.output.string).to eq([
+      "What letter? \n",
+      "\e[32m  4) D\e[0m\n",
+      "  5) E\n",
+      "  6) F\n",
+      "  Choose 1-8 [4]: ",
+      "\n\e[90m(Press tab/right or left to reveal more choices)\e[0m",
+      "\e[A\e[1G\e[18C",
+      "\e[1000D\e[K\e[1A" * 4 + "\e[1000D\e[K\e[J",
+      "What letter? \e[32mD\e[0m\n"
+    ].join)
+  end
+
+  it "shows pages matching input" do
+    prompt = TTY::TestPrompt.new
+    choices = %w(A B C D E F G H)
+    prompt.input << "11\n\b\n"
+    prompt.input.rewind
+    value = prompt.enum_select("What letter?", choices, per_page: 3)
+    expect(value).to eq('A')
+    expect(prompt.output.string).to eq([
+      "What letter? \n",
+      "\e[32m  1) A\e[0m\n",
+      "  2) B\n",
+      "  3) C\n",
+      "  Choose 1-8 [1]: ",
+      "\n\e[90m(Press tab/right or left to reveal more choices)\e[0m",
+      "\e[A\e[1G\e[18C",
+      "\e[1000D\e[K\e[1A" * 4 + "\e[1000D\e[K\e[J",
+      "What letter? \n",
+      "\e[32m  1) A\e[0m\n",
+      "  2) B\n",
+      "  3) C\n",
+      "  Choose 1-8 [1]: 1",
+      "\n\e[90m(Press tab/right or left to reveal more choices)\e[0m",
+      "\e[A\e[1G\e[19C",
+      "\e[1000D\e[K\e[1A" * 4 + "\e[1000D\e[K\e[J",
+      "What letter? \n",
+      "\e[32m  1) A\e[0m\n",
+      "  2) B\n",
+      "  3) C\n",
+      "  Choose 1-8 [1]: 11",
+      "\n\e[90m(Press tab/right or left to reveal more choices)\e[0m",
+      "\e[A\e[1G\e[20C",
+      "\e[1000D\e[K\e[1A" * 4 + "\e[1000D\e[K\e[J",
+      "What letter? \n",
+      "\e[32m  1) A\e[0m\n",
+      "  2) B\n",
+      "  3) C\n",
+      "  Choose 1-8 [1]: \n",
+      "\e[31m>>\e[0m Please enter a valid number",
+      "\n\e[90m(Press tab/right or left to reveal more choices)\e[0m",
+      "\e[A\e[1G\e[A\e[1G\e[18C",
+      "\e[1000D\e[K\e[1A" * 4 + "\e[1000D\e[K\e[J",
+      "What letter? \n",
+      "\e[32m  1) A\e[0m\n",
+      "  2) B\n",
+      "  3) C\n",
+      "  Choose 1-8 [1]: \n",
+      "\e[31m>>\e[0m Please enter a valid number",
+      "\n\e[90m(Press tab/right or left to reveal more choices)\e[0m",
+      "\e[A\e[1G\e[A\e[1G\e[18C",
+      "\e[1000D\e[K\e[1A" * 4 + "\e[1000D\e[K\e[J",
+      "What letter? \e[32mA\e[0m\n"
+    ].join)
+  end
+
+  it "switches through pages with tab key" do
+    prompt = TTY::TestPrompt.new
+    choices = %w(A B C D E F G H)
+    prompt.input << "\t\n"
+    prompt.input.rewind
+    value = prompt.enum_select("What letter?") do |menu|
+              menu.default 4
+              menu.per_page 3
+              menu.choices choices
+            end
+    expect(value).to eq('D')
+    expect(prompt.output.string).to eq([
+      "What letter? \n",
+      "\e[32m  4) D\e[0m\n",
+      "  5) E\n",
+      "  6) F\n",
+      "  Choose 1-8 [4]: ",
+      "\n\e[90m(Press tab/right or left to reveal more choices)\e[0m",
+      "\e[A\e[1G\e[18C",
+      "\e[1000D\e[K\e[1A" * 4 + "\e[1000D\e[K\e[J",
+      "What letter? \n",
+      "  7) G\n",
+      "  8) H\n",
+      "  Choose 1-8 [4]: ",
+      "\n\e[90m(Press tab/right or left to reveal more choices)\e[0m",
+      "\e[A\e[1G\e[18C",
+      "\e[1000D\e[K\e[1A" * 3 + "\e[1000D\e[K\e[J",
+      "What letter? \e[32mD\e[0m\n"
     ].join)
   end
 end
