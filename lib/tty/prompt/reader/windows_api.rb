313e0fe4204f00b491c8b9ecce54f9977b9fff36
Piotr Murach <pmurach@gmail.com>
2017-01-04 22:25:41 +0000

    Change to specify receiver.

--- lib/tty/prompt/reader/windows_api.rb
+++ lib/tty/prompt/reader/windows_api.rb
@@ -8,16 +8,16 @@ module TTY
       module WindowsAPI
         include Fiddle
 
-        CRT_HANDLE = Handle.new("msvcrt") rescue Handle.new("crtdll")
+        CRT_HANDLE = Fiddle::Handle.new("msvcrt") rescue Fiddle::Handle.new("crtdll")
 
         def getch
-          @@getch ||= Function.new(CRT_HANDLE["_getch"], [], TYPE_INT)
+          @@getch ||= ::Function.new(CRT_HANDLE["_getch"], [], TYPE_INT)
           @@getch.call
         end
         module_function :getch
 
         def getche
-          @@getche ||= Function.new(CRT_HANDLE["_getche"], [], TYPE_INT)
+          @@getche ||= ::Function.new(CRT_HANDLE["_getche"], [], TYPE_INT)
           @@getche.call
         end
         module_function :getche
