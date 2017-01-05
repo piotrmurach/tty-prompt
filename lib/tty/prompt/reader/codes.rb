fe8de6d4c7f9a4eecde972741934002d2ef76a88
Piotr Murach <pmurach@gmail.com>
2017-01-05 22:29:21 +0000

    Change codes to bytes.

--- lib/tty/prompt/reader/codes.rb
+++ lib/tty/prompt/reader/codes.rb
@@ -4,77 +4,104 @@ module TTY
   class Prompt
     class Reader
       module Codes
-        BACKSPACE = "\x7f"
-        DELETE   = "\004"
-        ESCAPE   = "\e"
-        LINEFEED = "\n"
-        RETURN   = "\r"
-        SPACE    = " "
-        TAB      = "\t"
+        def keys
+          {
+            tab:       ["\t".ord],
+            enter:     ["\n".ord],
+            return:    ["\r".ord],
+            escape:    ["\e".ord],
+            space:     [" ".ord],
+            backspace: ["\x7f".ord],
+            insert:    "\e[2~".bytes.to_a,
+            delete:    "\e[3~".bytes.to_a,
 
-        KEY_UP     = "[A"
-        KEY_DOWN   = "[B"
-        KEY_RIGHT  = "[C"
-        KEY_LEFT   = "[D"
-        KEY_CLEAR  = "[E"
-        KEY_END    = "[F"
-        KEY_HOME   = "[H"
-        KEY_DELETE = "[3"
+            up:     "\e[A".bytes.to_a,
+            down:   "\e[B".bytes.to_a,
+            right:  "\e[C".bytes.to_a,
+            left:   "\e[D".bytes.to_a,
+            clear:  "\e[E".bytes.to_a,
+            end:    "\e[F".bytes.to_a,
+            home:   "\e[H".bytes.to_a,
 
-        KEY_UP_XTERM     = "OA"
-        KEY_DOWN_XTERM   = "OB"
-        KEY_RIGHT_XTERM  = "OC"
-        KEY_LEFT_XTERM   = "OD"
-        KEY_CLEAR_XTERM  = "OE"
-        KEY_END_XTERM    = "OF"
-        KEY_HOME_XTERM   = "OH"
-        KEY_DELETE_XTERM = "O3"
+            ctrl_a: [?\C-a.ord],
+            ctrl_b: [?\C-b.ord],
+            ctrl_c: [?\C-c.ord],
+            ctrl_d: [?\C-d.ord],
+            ctrl_e: [?\C-e.ord],
+            ctrl_f: [?\C-f.ord],
+            ctrl_g: [?\C-g.ord],
+            ctrl_h: [?\C-h.ord],
+            ctrl_i: [?\C-i.ord],
+            ctrl_j: [?\C-j.ord],
+            ctrl_k: [?\C-k.ord],
+            ctrl_l: [?\C-l.ord],
+            ctrl_m: [?\C-m.ord],
+            ctrl_n: [?\C-n.ord],
+            ctrl_o: [?\C-o.ord],
+            ctrl_p: [?\C-p.ord],
+            ctrl_q: [?\C-q.ord],
+            ctrl_r: [?\C-r.ord],
+            ctrl_s: [?\C-s.ord],
+            ctrl_t: [?\C-t.ord],
+            ctrl_u: [?\C-u.ord],
+            ctrl_v: [?\C-v.ord],
+            ctrl_w: [?\C-w.ord],
+            ctrl_x: [?\C-x.ord],
+            ctrl_y: [?\C-y.ord],
+            ctrl_z: [?\C-z.ord],
 
-        KEY_UP_SHIFT    = "[a"
-        KEY_DOWN_SHIFT  = "[b"
-        KEY_RIGHT_SHIFT = "[c"
-        KEY_LEFT_SHIFT  = "[d"
-        KEY_CLEAR_SHIFT = "[e"
+            f1_xterm: "\eOP".bytes.to_a,
+            f2_xterm: "\eOQ".bytes.to_a,
+            f3_xterm: "\eOR".bytes.to_a,
+            f4_xterm: "\eOS".bytes.to_a,
 
-        KEY_UP_CTRL    = "0a"
-        KEY_DOWN_CTRL  = "0b"
-        KEY_RIGHT_CTRL = "0c"
-        KEY_LEFT_CTRL  = "0d"
-        KEY_CLEAR_CTRL = "0e"
+            f1:  "\e[11~".bytes.to_a,
+            f2:  "\e[12~".bytes.to_a,
+            f3:  "\e[13~".bytes.to_a,
+            f4:  "\e[14~".bytes.to_a,
+            f5:  "\e[15~".bytes.to_a,
+            f6:  "\e[17~".bytes.to_a,
+            f7:  "\e[18~".bytes.to_a,
+            f8:  "\e[19~".bytes.to_a,
+            f9:  "\e[20~".bytes.to_a,
+            f10: "\e[21~".bytes.to_a,
+            f11: "\e[23~".bytes.to_a,
+            f12: "\e[24~".bytes.to_a
+          }
+        end
+        module_function :keys
 
-        CTRL_J = "\x0A"
-        CTRL_N = "\x0E"
-        CTRL_K = "\x0B"
-        CTRL_P = "\x10"
-        SIGINT = "\x03"
-        CTRL_C = "\x03"
-        CTRL_H = "\b"
-        CTRL_L = "\f"
+        # KEY_UP_XTERM     = "OA"
+        # KEY_DOWN_XTERM   = "OB"
+        # KEY_RIGHT_XTERM  = "OC"
+        # KEY_LEFT_XTERM   = "OD"
+        # KEY_CLEAR_XTERM  = "OE"
+        # KEY_END_XTERM    = "OF"
+        # KEY_HOME_XTERM   = "OH"
+        # KEY_DELETE_XTERM = "O3"
 
-        F1_XTERM = "OP"
-        F2_XTERM = "OQ"
-        F3_XTERM = "OR"
-        F4_XTERM = "OS"
+        # KEY_UP_SHIFT    = "[a"
+        # KEY_DOWN_SHIFT  = "[b"
+        # KEY_RIGHT_SHIFT = "[c"
+        # KEY_LEFT_SHIFT  = "[d"
+        # KEY_CLEAR_SHIFT = "[e"
 
-        F1_GNOME = "[11~"
-        F2_GNOME = "[12~"
-        F3_GNOME = "[13~"
-        F4_GNOME = "[14~"
+        # KEY_UP_CTRL    = "0a"
+        # KEY_DOWN_CTRL  = "0b"
+        # KEY_RIGHT_CTRL = "0c"
+        # KEY_LEFT_CTRL  = "0d"
+        # KEY_CLEAR_CTRL = "0e"
 
-        F1_WIN = "[[A"
-        F2_WIN = "[[B"
-        F3_WIN = "[[C"
-        F4_WIN = "[[D"
-        F5_WIN = "[[E"
+        # F1_XTERM = "OP"
+        # F2_XTERM = "OQ"
+        # F3_XTERM = "OR"
+        # F4_XTERM = "OS"
 
-        F5 =  "[15~"
-        F6 =  "[17~"
-        F7 =  "[18~"
-        F8 =  "[19~"
-        F9 =  "[20~"
-        F10 = "[21~"
-        F11 = "[23~"
-        F12 = "[24~"
+        # F1_WIN = "[[A"
+        # F2_WIN = "[[B"
+        # F3_WIN = "[[C"
+        # F4_WIN = "[[D"
+        # F5_WIN = "[[E"
       end # Codes
     end # Reader
   end # Prompt
