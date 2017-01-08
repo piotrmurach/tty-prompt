f9319c08ce30566ea4f1d8cd298a3299be25988f
Piotr Murach <pmurach@gmail.com>
2017-01-08 20:22:56 +0000

    Add cross platform unicode symbols.

--- lib/tty/prompt/symbols.rb
+++ lib/tty/prompt/symbols.rb
@@ -2,18 +2,57 @@
 
 module TTY
   class Prompt
+    # Cross platform common Unicode symbols.
+    #
+    # @api public
     module Symbols
-      SPACE   = " "
-      SUCCESS = "✓"
-      FAILURE = "✘"
+      KEYS = {
+        tick: '✓',
+        cross: '✘',
+        star: '★',
+        pointer: '‣',
+        line: '─',
+        pipe: '|',
+        handle: 'O',
+        ellipsis: '…',
+        radio_on: '⬢',
+        radio_off: '⬡',
+        checkbox_on: '☒',
+        checkbox_off: '☐',
+        circle_on: 'ⓧ',
+        circle_off: 'Ⓘ'
+      }.freeze
 
-      ITEM_SECURE     = "•"
-      ITEM_SELECTED   = "‣"
-      RADIO_CHECKED   = "⬢"
-      RADIO_UNCHECKED = "⬡"
-      SLIDER_HANDLE   = 'O'
-      SLIDER_RANGE    = '-'
-      SLIDER_END      = '|'
+      WIN_KEYS = {
+        tick: '√',
+        cross: '×',
+        star: '*',
+        pointer: '>',
+        line: '─',
+        pipe: '|',
+        ellipsis: '...',
+        radio_on: '(*)',
+        radio_off: '( )',
+        checkbox_on: '[×]',
+        checkbox_off: '[ ]',
+        circle_on: '(x)',
+        circle_off: '( )'
+      }.freeze
+
+      def symbols
+        @symbols ||= windows? ? WIN_KEYS : KEYS
+      end
+      module_function :symbols
+
+      # Check if Windowz
+      #
+      # @return [Boolean]
+      #
+      # @api public
+      def windows?
+        File::ALT_SEPARATOR == "\\"
+      end
+      module_function :windows?
     end # Symbols
   end # Prompt
 end # TTY
