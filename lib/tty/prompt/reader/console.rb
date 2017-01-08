4e694fbc309d39a79fc3ec3a019cc15bc45cd841
Piotr Murach <pmurach@gmail.com>
2017-01-08 14:20:36 +0000

    Add console escape codes and keys calls.

--- lib/tty/prompt/reader/console.rb
+++ lib/tty/prompt/reader/console.rb
@@ -1,15 +1,40 @@
 # encoding: utf-8
 
+require 'tty/prompt/reader/codes'
 require 'tty/prompt/reader/mode'
 
 module TTY
   class Prompt
     class Reader
       class Console
+        ESC = "\e".freeze
+        CSI = "\e[".freeze
+
+        attr_reader :mode
+
+        attr_reader :input
 
         def initialize(input)
           @input = input
           @mode  = Mode.new
+        end
+
+        # Escape codes
+        #
+        # @return [Array[Integer]]
+        #
+        # @api public
+        def escape_codes
+          [[ESC.ord], CSI.bytes.to_a]
+        end
+
+        # Key codes
+        #
+        # @return [Hash[Symbol]]
+        #
+        # @api public
+        def keys
+          Codes.keys
         end
 
         # Get a character from console with echo
@@ -26,13 +51,6 @@ module TTY
             mode.echo(options[:echo]) { input.getc }
           end
         end
-
-        private
-
-        attr_reader :mode
-
-        attr_reader :input
-
       end # Console
     end # Reader
   end # Prompt
--- lib/tty/prompt/reader/win_console.rb
+++ lib/tty/prompt/reader/win_console.rb
@@ -1,12 +1,33 @@
 # encoding: utf-8
 
+require 'tty/prompt/reader/codes'
+
 module TTY
   class Prompt
     class Reader
       class WinConsole
+        ESC = "\e".freeze
 
         def initialize
           require 'tty/prompt/reader/windows_api'
+        end
+
+        # Escape codes
+        #
+        # @return [Array[Integer]]
+        #
+        # @api public
+        def escape_codes
+          [[0], [ESC.ord], [224]]
+        end
+
+        # Key codes
+        #
+        # @return [Hash[Symbol]]
+        #
+        # @api public
+        def keys
+          Codes.win_keys
         end
 
         # Get a character from console with echo
