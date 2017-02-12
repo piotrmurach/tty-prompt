add628302adec28e4a5e9470ddf7185ccec93215
Piotr Murach <pmurach@gmail.com>
2017-02-12 21:11:33 +0000

    Add inputs example.

---
+++ examples/inputs.rb
@@ -0,0 +1,11 @@
+# encoding: utf-8
+
+require 'tty-prompt'
+
+prompt = TTY::Prompt.new
+
+prompt.ask('What is your name?', default: ENV['USER'])
+prompt.yes?('Do you like Ruby?')
+prompt.mask("What is your secret?")
+prompt.select("Choose your destiny?", %w(Scorpion Kano Jax))
+
