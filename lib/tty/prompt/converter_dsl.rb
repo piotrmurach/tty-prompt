fbc1f3f990c0dec714e00a72686b73e96f39b4a5
Piotr Murach <pmurach@gmail.com>
2017-02-18 12:02:27 +0000

    Change to directly expose method.

--- lib/tty/prompt/converter_dsl.rb
+++ lib/tty/prompt/converter_dsl.rb
@@ -5,14 +5,13 @@ require_relative 'converter_registry'
 module TTY
   class Prompt
     module ConverterDSL
-      def self.extended(base)
-        attr_reader :converter_registry
-
-        base.instance_variable_set(:@converter_registry, ConverterRegistry.new)
+      def converter_registry
+        @converter_registry ||= ConverterRegistry.new
       end
 
       def converter(name, &block)
-        converter_registry.register(name, &block)
+        @converter_registry = converter_registry.register(name, &block)
+        self
       end
     end # ConverterDSL
   end # Prompt
