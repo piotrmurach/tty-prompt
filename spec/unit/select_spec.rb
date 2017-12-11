1f968cc80012ca106c1ae50d6d149e4aca0973c9
Jonas Mueller <jonas@muellers.one>
2017-12-11 11:12:10 +0100

    Add specs for wrapping behaviour

--- spec/unit/select_spec.rb
+++ spec/unit/select_spec.rb
@@ -6,6 +6,27 @@ RSpec.describe TTY::Prompt, '#select' do
 
   let(:symbols) { TTY::Prompt::Symbols.symbols }
 
+  def output_helper(question, choices, active, init: false)
+    [].tap do |out|
+      out << if init
+               "\e[?25l#{question}" + \
+               " \e[90m(Use arrow keys, press Enter to select)\e[0m\n"
+             else
+               "#{question} \n"
+             end
+      choices.each do |c|
+        out << if c == active
+                 "\e[32m#{symbols[:pointer]} #{c}\e[0m"
+               else
+                 "  #{c}"
+               end
+        out.last << "\n" unless c == choices.last
+      end
+      out << "\e[2K\e[1G\e[1A" * choices.count
+      out << "\e[2K\e[1G"
+    end
+  end
+
   it "selects by default first option" do
     choices = %w(Large Medium Small)
     prompt.input << "\r"
@@ -295,7 +316,7 @@ RSpec.describe TTY::Prompt, '#select' do
     choices = %w(A B C D E F G H)
     prompt.input << "\r"
     prompt.input.rewind
-    value = prompt.select('What letter?') do |menu|
+    value = prompt.select("What letter?") do |menu|
               menu.per_page 3
               menu.page_help '(Wiggle thy finger up or down to see more)'
               menu.default 4
@@ -313,6 +334,40 @@ RSpec.describe TTY::Prompt, '#select' do
       "\e[2K\e[1G",
       "What letter? \e[32mD\e[0m\n\e[?25h",
     ].join)
+  end
+
+  it "wraps around when reaching the end of the list" do
+    prompt = TTY::TestPrompt.new
+    choices = %w(A B C)
+    prompt.on(:keypress) { |e| prompt.trigger(:keydown) if e.value == "j" }
+    prompt.input << "j" << "j" << "j" << "\r"
+    prompt.input.rewind
+    value = prompt.select("What letter?", choices)
+    expect(value).to eq("A")
+    expect(prompt.output.string).to eq((
+      output_helper("What letter?", choices, "A", init: true) +
+      output_helper("What letter?", choices, "B") +
+      output_helper("What letter?", choices, "C") +
+      output_helper("What letter?", choices, "A") +
+      ["What letter? \e[32mA\e[0m\n\e[?25h"]
+    ).join)
+  end
+
+  it "doesnt wrap around when configured not to" do
+    prompt = TTY::TestPrompt.new
+    choices = %w(A B C)
+    prompt.on(:keypress) { |e| prompt.trigger(:keydown) if e.value == "j" }
+    prompt.input << "j" << "j" << "j" << "\r"
+    prompt.input.rewind
+    value = prompt.select("What letter?", choices, wraparound: false)
+    expect(value).to eq("C")
+    expect(prompt.output.string).to eq((
+      output_helper("What letter?", choices, "A", init: true) +
+      output_helper("What letter?", choices, "B") +
+      output_helper("What letter?", choices, "C") +
+      output_helper("What letter?", choices, "C") +
+      ["What letter? \e[32mC\e[0m\n\e[?25h"]
+    ).join)
   end
 
   it "verifies default index format" do
