cb13a037f5e454d9b3a9aaafa1f34a33beab1b8c
Piotr Murach <pmurach@gmail.com>
2017-12-31 16:45:45 +0100

    Change to simplify example

--- examples/slider.rb
+++ examples/slider.rb
@@ -3,4 +3,4 @@
 require_relative "../lib/tty-prompt"
 
 prompt = TTY::Prompt.new
-prompt.slider("Volume", min: 0, max: 100, step: 5, format: "|:slider| %d%")
+prompt.slider("Volume", max: 100, step: 5, default: 75, format: "|:slider| %d%")
