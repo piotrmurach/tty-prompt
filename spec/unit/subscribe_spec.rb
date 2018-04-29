4c8858a95d2119f9f3c529fbd55dc3d39867a0db
Piotr Murach <pmurach@gmail.com>
2018-04-29 14:54:55 +0100

    Add spec to ensure key events subscription works correctly and close issue #83

---
+++ spec/unit/subscribe_spec.rb
@@ -0,0 +1,20 @@
+RSpec.describe TTY::Prompt, '#subscribe' do
+  it "subscribes to key events only for the current prompt" do
+    prompt = TTY::TestPrompt.new
+    uuid = '14c3b412-e0c5-4ff5-9cd8-25ec3f18c702'
+    prompt.input << "3\n#{uuid}\n"
+    prompt.input.rewind
+    keys = []
+
+    prompt.on(:keypress) do |event|
+      keys << :enter if event.key.name == :enter
+    end
+
+    letter = prompt.enum_select('Select something', ('A'..'Z').to_a)
+    id = prompt.ask('Request ID?')
+
+    expect(letter).to eq('C')
+    expect(id).to eq(uuid)
+    expect(keys).to eq([:enter, :enter])
+  end
+end
