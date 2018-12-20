f2a9f6d5fa02c4b4f5b577f30e9f4050f7681a25
Piotr Murach <pmurach@gmail.com>
2018-12-20 21:59:15 +0100

    Change enum_select example to use symbols as choices

--- examples/enum_select.rb
+++ examples/enum_select.rb
@@ -3,5 +3,5 @@
 require_relative "../lib/tty-prompt"
 
 prompt = TTY::Prompt.new
-choices = %w(/bin/nano /usr/bin/vim.basic /usr/bin/vim.tiny)
+choices = %i(/bin/nano /usr/bin/vim.basic /usr/bin/vim.tiny)
 prompt.enum_select('Select an editor', choices, default: 2)
