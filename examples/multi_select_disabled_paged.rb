38bb2f588f3615dc657989509a5d9f2b71bdb70d
Piotr Murach <pmurach@gmail.com>
2019-01-30 22:18:44 +0000

    Add multi_select prompt pagination of disabled items example

---
+++ examples/multi_select_disabled_paged.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+require_relative "../lib/tty-prompt"
+
+prompt = TTY::Prompt.new
+
+numbers = [
+  {name: '1', disabled: 'out'},
+  '2',
+  {name: '3', disabled: 'out'},
+  '4',
+  '5',
+  {name: '6', disabled: 'out'},
+  '7',
+  '8',
+  '9',
+  {name: '10', disabled: 'out'}
+]
+
+answer = prompt.multi_select('Which letter?', numbers, per_page: 4, cycle: true)
+
+puts answer.inspect
