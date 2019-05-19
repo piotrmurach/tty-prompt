36cc6a5c5934e4c2f2ffbc3afa9d13f03a8ee020
Piotr Murach <pmurach@gmail.com>
2019-05-19 13:31:07 +0100

    Add example of auto hints in expand prompt in ref #60

---
+++ examples/expand_auto.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+require_relative "../lib/tty-prompt"
+
+choices = [{
+  key: 'y',
+  name: 'overwrite this file',
+  value: :yes
+}, {
+  key: 'n',
+  name: 'do not overwrite this file',
+  value: :no
+}, {
+  key: 'a',
+  name: 'overwrite this file and all later files',
+  value: :all
+}, {
+  key: 'd',
+  name: 'show diff',
+  value: :diff
+}, {
+  key: 'q',
+  name: 'quit; do not overwrite this file ',
+  value: :quit
+}]
+
+prompt = TTY::Prompt.new
+
+prompt.expand('Overwrite Gemfile?', choices, auto_hint: true)
