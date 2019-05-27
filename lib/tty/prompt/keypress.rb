87e95cb4c179070ec73085cd0a67fda14b4a85cd
Piotr Murach <pmurach@gmail.com>
2019-05-27 13:17:05 +0100

    Change to extract method call and remove timeout

--- lib/tty/prompt/keypress.rb
+++ lib/tty/prompt/keypress.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 require_relative 'question'
-require_relative 'timeout'
 require_relative 'timer'
 
 module TTY
@@ -22,17 +21,8 @@ module TTY
           (@timeout != UndefinedSetting && @timeout < 1) ? @timeout : 1
         }
         @countdown = @timeout
-        @interval_handler = -> (time) {
-          unless @done
-            question = render_question
-            line_size = question.size
-            total_lines = @prompt.count_screen_lines(line_size)
-            @prompt.print(refresh(question.lines.count, total_lines))
-            countdown(time)
-            @prompt.print(render_question)
-          end
-        }
-        @scheduler = Timeout.new(interval_handler: @interval_handler)
+        time = timeout? ? Float(@timeout) : nil
+        @timer = Timer.new(time, Float(@interval))
 
         @prompt.subscribe(self)
       end
@@ -55,10 +45,8 @@ module TTY
       def keypress(event)
         if any_key?
           @done = true
-          @scheduler.cancel
         elsif @keys.is_a?(Array) && @keys.include?(event.key.name)
           @done = true
-          @scheduler.cancel
         else
           @done = false
         end
@@ -70,14 +58,22 @@ module TTY
         header
       end
 
-      def process_input(question)
-        time = timeout? ? Float(@timeout) : nil
-        timer = Timer.new(time, Float(@interval))
+      def interval_handler(time)
+        return if @done
 
+        question = render_question
+        line_size = question.size
+        total_lines = @prompt.count_screen_lines(line_size)
+        @prompt.print(refresh(question.lines.count, total_lines))
+        countdown(time)
         @prompt.print(render_question)
-        timer.while_remaining do |remaining|
+      end
+
+      def process_input(question)
+        @prompt.print(render_question)
+        @timer.while_remaining do |remaining|
           break if @done
-          @interval_handler.(remaining.round) if timeout?
+          interval_handler(remaining.round) if timeout?
           @input = @prompt.read_keypress(nonblock: true)
         end
 
@@ -86,19 +82,6 @@ module TTY
 
       def refresh(lines, lines_to_clear)
         @prompt.clear_lines(lines)
-      end
-
-      # Wait for keypress or timeout
-      #
-      # @api private
-      def time(&job)
-        if timeout?
-          time = Float(@timeout)
-          interval = Float(@interval)
-          @scheduler.timeout(time, interval, &job)
-        else
-          job.()
-        end
       end
     end # Keypress
   end # Prompt
