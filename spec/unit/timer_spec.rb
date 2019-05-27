7ae82720385733c2610abda0fd2116e4d833ca97
Piotr Murach <pmurach@gmail.com>
2019-05-27 12:53:11 +0100

    Change to ensure all values are yielded

--- spec/unit/timer_spec.rb
+++ spec/unit/timer_spec.rb
@@ -1,14 +1,14 @@
 # frozen_string_literal: true
 
 RSpec.describe TTY::Prompt::Timer do
-  it "" do
+  it "times out loop execution" do
     timer = TTY::Prompt::Timer.new(0.03, 0.01)
 
     yielded = []
 
     timer.while_remaining do |remaining|
-      sleep(0.01)
       yielded << remaining
+      sleep(0.01)
     end
 
     expect(yielded).to match_array([
