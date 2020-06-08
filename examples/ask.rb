ef2691d7b25e047ccff919ee8044bc47fe878c4c
Piotr Murach <pmurach@gmail.com>
2020-06-08 17:42:54 +0100

    Change example to show color configuration with a callable object

--- examples/ask.rb
+++ examples/ask.rb
@@ -1,7 +1,11 @@
 # frozen_string_literal: true
 
+require "pastel"
 require_relative "../lib/tty-prompt"
 
 prompt = TTY::Prompt.new
+pastel = Pastel.new
+notice = pastel.cyan.bold.detach
 
-prompt.ask('What is your name?', default: ENV['USER'])
+prompt.ask("What is your name?", default: ENV["USER"],
+           active_color: notice, help_color: notice)
