b7f1f024ee4219f61bf725bae2019f0605f0c875
Piotr Murach <pmurach@gmail.com>
2020-06-12 22:52:03 +0100

    Add setting to allow full diff comparison

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -37,6 +37,7 @@ RSpec.configure do |config|
 
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+    expectations.max_formatted_output_length = nil
   end
 
   config.mock_with :rspec do |mocks|
