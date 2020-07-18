dd87b1471eaf26605ceda3b528d933cffe68c4a6
Piotr Murach <pmurach@gmail.com>
2020-07-18 11:02:24 +0100

    Change all errors to inherit from the common Error class

--- lib/tty/prompt.rb
+++ lib/tty/prompt.rb
@@ -28,23 +28,25 @@ module TTY
   class Prompt
     extend Forwardable
 
+    Error = Class.new(StandardError)
+
     # Raised when wrong parameter is used to configure prompt
-    ConfigurationError = Class.new(StandardError)
+    ConfigurationError = Class.new(Error)
 
     # Raised when type conversion cannot be performed
-    ConversionError = Class.new(StandardError)
+    ConversionError = Class.new(Error)
 
     # Raised when the passed in validation argument is of wrong type
-    ValidationCoercion = Class.new(TypeError)
+    ValidationCoercion = Class.new(Error)
 
     # Raised when the required argument is not supplied
-    ArgumentRequired = Class.new(ArgumentError)
+    ArgumentRequired = Class.new(Error)
 
     # Raised when the argument validation fails
-    ArgumentValidation = Class.new(ArgumentError)
+    ArgumentValidation = Class.new(Error)
 
     # Raised when the argument is not expected
-    InvalidArgument = Class.new(ArgumentError)
+    InvalidArgument = Class.new(Error)
 
     # @api private
     attr_reader :input
