5cb783dd51924806118f0c4cbfa8da6b0320d3b2
Piotr Murach <pmurach@gmail.com>
2021-04-01 17:48:07 +0200

    Add tests to cover searching for a choice by value or key attribute

--- spec/unit/choices/find_by_spec.rb
+++ spec/unit/choices/find_by_spec.rb
@@ -1,10 +1,34 @@
 # frozen_string_literal: true
 
 RSpec.describe TTY::Prompt::Choices, "#find_by" do
-  it "finds a matching choice by key name" do
-    collection = [{name: "large"},{name: "medium"},{name: "small"}]
-    choice = TTY::Prompt::Choice.from(name: "small")
+  let(:collection) {
+    [
+      { name: "large", value: "L", key: "l" },
+      { name: "medium", value: "M", key: "m" },
+      { name: "small", value: "S", key: "s" }
+    ]
+  }
+
+  it "finds no matching choice" do
+    choices = described_class[*collection]
+    expect(choices.find_by(:name, "unknown")).to eq(nil)
+  end
+
+  it "finds a matching choice by :name key" do
+    choice = TTY::Prompt::Choice.from({ name: "small", value: "S", key: "s" })
     choices = described_class[*collection]
     expect(choices.find_by(:name, "small")).to eq(choice)
+  end
+
+  it "finds a matching choice by :value key" do
+    choice = TTY::Prompt::Choice.from({ name: "medium", value: "M", key: "m" })
+    choices = described_class[*collection]
+    expect(choices.find_by(:value, "M")).to eq(choice)
+  end
+
+  it "finds a matching choice by :key key" do
+    choice = TTY::Prompt::Choice.from({ name: "large", value: "L", key: "l" })
+    choices = described_class[*collection]
+    expect(choices.find_by(:key, "l")).to eq(choice)
   end
 end
